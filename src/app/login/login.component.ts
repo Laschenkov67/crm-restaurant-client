c2a4d6d521a1e371a1be5778e6eb2d54ceb23932
laschenkov.artem <laschenkov.artem@otr.ru>
2020-03-03 14:41:20 +0300

    validation login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  formLogin: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    //Создаем валидацию формы
+    this.formLogin = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
   }
 
+  onLogin() {
+    console.log("Отправка данных для авторизации");
+  }
 }

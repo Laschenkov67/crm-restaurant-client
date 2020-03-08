6c2471556f878ad6ca37a6058e3c31f54baa1ab2
laschenkov.artem <laschenkov.artem@otr.ru>
2020-03-08 23:47:10 +0300

    load images and delete category

--- src/app/categories/categories-form/categories-form.component.ts
+++ src/app/categories/categories-form/categories-form.component.ts
@@ -1,8 +1,9 @@
+import { Category } from './../../interfaces/categories/category.interface';
 import { MaterialService } from './../../services/material.service';
 import { CategoriesService } from './../../services/categories.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import {switchMap} from 'rxjs/operators';
 import {of} from 'rxjs';
 
@@ -13,43 +14,106 @@ import {of} from 'rxjs';
 })
 export class CategoriesFormComponent implements OnInit {
 
+  @ViewChild('input', {static:true}) inputRef: ElementRef
   form: FormGroup
-  isNew = true;
+  image: File
+  imagePreview: string | ArrayBuffer = ''
+  isNew = true
+  category: Category
 
-  constructor(
-    private route: ActivatedRoute,
-    private categoriesService: CategoriesService) {}
+  constructor(private route: ActivatedRoute,
+              private categoriesService: CategoriesService,
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl(null, Validators.required)
-    });
+    })
 
-    this.form.disable();
+    this.form.disable()
 
     this.route.params
       .pipe(
-        switchMap((params: Params) => {
-          if (params["id"]) {
-            this.isNew = false;
-            return this.categoriesService.getById(params["id"]);
-          }
+        switchMap(
+          (params: Params) => {
+            if (params['id']) {
+              this.isNew = false
+              return this.categoriesService.getById(params['id'])
+            }
 
-          return of(null);
-        })
+            return of(null)
+          }
+        )
       )
       .subscribe(
-        category => {
+        (category: Category) => {
           if (category) {
+            this.category = category
             this.form.patchValue({
               name: category.name
-            });
-            MaterialService.updateTextInputs();
+            })
+            this.imagePreview = category.imageSrc
+            MaterialService.updateTextInputs()
           }
 
-          this.form.enable();
+          this.form.enable()
         },
         error => MaterialService.toast(error.error.message)
-      );
+      )
+  }
+
+  triggerClick() {
+    this.inputRef.nativeElement.click()
+  }
+
+  onFileUpload(event: any) {
+    const file = event.target.files[0]
+    this.image = file
+
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      this.imagePreview = reader.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  onSubmit() {
+    let obs$
+    this.form.disable()
+
+    if (this.isNew) {
+      obs$ = this.categoriesService.create(this.form.value.name, this.image)
+    } else {
+      obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+    }
+
+    obs$.subscribe(
+      category => {
+        this.category = category
+        MaterialService.toast('Изменения сохранены.')
+        this.form.enable()
+      },
+      error => {
+        MaterialService.toast(error.error.message)
+        this.form.enable()
+      }
+    )
+  }
+
+
+  deleteCategory() {
+    const decision = window.confirm(`Вы уверены, что хотите удалить категорию "${this.category.name}"`)
+
+    if (decision) {
+      this.categoriesService.delete(this.category._id)
+        .subscribe(
+          response => MaterialService.toast(response.message),
+          error => MaterialService.toast(error.error.message),
+          () => this.router.navigate(['/categories'])
+        )
+    }
   }
 }
